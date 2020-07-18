69a400c491ffe09ce48a46360428f5850d496eab
gufronsp <gufronsp@gmail.com>
2020-07-18 14:54:46 +0700

    Fix error vuetify initialize

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import Vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify';
 // import assets
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 // import pages/components
 import Login from '@/pages/Login'
+import Index from '@/pages/Index'
 
 Vue.config.productionTip = false
 
@@ -17,12 +18,17 @@ const Router = new VueRouter({
       path: '/login', 
       components: Login,
       name: 'login'
+    },
+    {
+      path: '/index',
+      components: Index,
+      name: 'index'
     }
   ]
 })
 
 new Vue({
   Router,
-  Vuetify,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
